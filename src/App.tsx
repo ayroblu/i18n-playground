ec1649843d0fa134da622ce48ae53abec6fc5c73
Ben Lu <ayroblu@gmail.com>
2020-04-25 14:35:52 +0100

    Create a sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import styles from './App.module.css';
+import React from "react";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className={styles.app} lang='en'>
+    <div className={styles.app} lang="en">
       <ul className={styles.sideBar}>
         <li>English</li>
         <li>Chinese (left to right)</li>
@@ -11,9 +11,11 @@ function App() {
         <li>Arabic</li>
         <li>Mongolian</li>
       </ul>
-      <h1>This is my header title</h1>
-      <p>This is my child content</p>
-      <p lang="zh">写出来的中文</p>
+      <section className={styles.content}>
+        <h1>This is my header title</h1>
+        <p>This is my child content</p>
+        <p lang="zh">写出来的中文</p>
+      </section>
     </div>
   );
 }
